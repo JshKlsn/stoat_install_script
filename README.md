1d7e0fbad087204d85aab8b7eb54294345a7e1b9
Sylphux <masip.pablo@gmail.com>
2026-02-19 23:54:19 +0100

    Clearer text, one line command in readme for simple use case

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a simple shell install script for Stoat, an open source alternative for discord.
 
-At the moment there is no support for packet managers so this script will install stoat from the release on github.
+At the moment there is no support for package managers so this script will install stoat from the specified release on github.
 
 # How to use ?
 
@@ -12,10 +12,19 @@ Check the latest release version, update it at the top of the script before laun
 
 Be sure to have installed all the dependencies and chromium beforehand.
 
+## Everything in one command
+
+If you want the simplest experience possible, and paste only one command for everything do :
+
+```shell
+git clone https://github.com/Sylphux/stoat_install_script && cd stoat_install_script && chmod +x stoat_install.sh && ./stoat_install.sh
+```
+
+This will clone the repo and install stoat.
+
 ## How to start the script
 
-
-```
+```shell
 chmod +x stoat_install.sh
 ./stoat_install.sh
 ```
@@ -23,6 +32,11 @@ chmod +x stoat_install.sh
 # About 
 
 This is not affiliated with the Stoat team.
-All filed downloaded comes from the official Stoat repos.
+All files downloaded comes from the official Stoat repos.
 
 Feel free to contact me for bug reports or advices.
+You can also give feedback on the reddit post : https://www.reddit.com/r/stoatchat/comments/1r98j9a/comment/o6bl1wm/
+
+Drop a star if it was useful for you ! :)
+
+Nothing was done with AI.
--- stoat_install.sh
+++ stoat_install.sh
@@ -1,16 +1,26 @@
-VERSION="1.3.0"
+# You can edit all these variables to customize your installation
+
+VERSION="1.3.0" # Please make sure this is the latest release available. I wont update the script every time.
 INSTALL_PATH="$HOME/.local/bin/"
 FILE="Stoat-linux-x64-$VERSION.zip"
 RELEASE_URL="https://github.com/stoatchat/for-desktop/releases/download/v$VERSION/Stoat-linux-x64-$VERSION.zip"
 ICON_URL="https://raw.githubusercontent.com/stoatchat/assets/refs/heads/main/desktop/icon.ico"
 ICON_PATH="$INSTALL_PATH/Stoat-linux-x64/icon.ico"
 DESKTOP_FILE="$HOME/.local/share/applications/stoat.desktop"
+
+# Do not edit this unless you know what you are doing
+
 echo "Welcome to this basic Stoat install/update script for linux."
-echo "Version will $VERSION will be downloaded and installed / updated. This can be changed in the beginning of the script."
 echo "Be sure to have chromium and all the required dependencies installed before proceeding."
+echo ""
+echo "Version $VERSION will be downloaded and installed / updated. This can be changed in the beginning of the script."
 echo "Please check for new versions at https://github.com/stoatchat/for-desktop/releases/"
 echo ""
-read -p "Press enter to continue or leave now: "
+read -sp "Continue ? (Y/n): " ask
+if [ "$ask" = "n" ]; then
+    echo "Exiting."
+    exit
+fi
 output=""
 wget -O $FILE $RELEASE_URL && echo "Downloaded $FILE" && output="success" || output="failed"
 if [ "$output" != "success" ]; then
@@ -29,11 +39,11 @@ echo "[Desktop Entry]" > $DESKTOP_FILE
 echo "Type=Application" >> $DESKTOP_FILE
 echo "Name=Stoat" >> $DESKTOP_FILE
 echo "GenericName=Stoat" >> $DESKTOP_FILE
-echo "Comment=Stoat sandbox" >> $DESKTOP_FILE
+echo "Comment=Open source user-first chat platform." >> $DESKTOP_FILE
 echo "Icon=$ICON_PATH" >> $DESKTOP_FILE
 echo "Exec=$INSTALL_PATH/Stoat-linux-x64/stoat-desktop" >> $DESKTOP_FILE
 echo "Categories=Network;InstantMessaging;" >> $DESKTOP_FILE
 echo "Terminal=false" >> $DESKTOP_FILE
 echo "Keywords=Chat;Messaging;Stoat;" >> $DESKTOP_FILE
 echo ""
-echo "Installation complete. You can now launch Stoat. You can also use this script to update the application by changing the release version at the top of the script."
+echo "Installation/update complete. You can now launch Stoat."
